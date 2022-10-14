ef6b3a14f1fd6fa25be661cc777ffcab87c6d749
Miguel Angel  Espinoza Salmoran <miguel.espinozas@coppel.com>
2022-10-14 15:19:21 -0600

    Se agregan los parametros a los modelos con sus respectivas funciones

--- demo/src/test/java/com/example/demo/models/Address.java
+++ demo/src/test/java/com/example/demo/models/Address.java
@@ -1,5 +1,28 @@
 package com.example.demo.models;
 
 public class Address {
-
+	
+	public String calle;
+	public String numero_interior;
+	public String colonia;
+	
+	public String getCalle() {
+		return calle;
+	}
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+	public String getNumero_interior() {
+		return numero_interior;
+	}
+	public void setNumero_interior(String numero_interior) {
+		this.numero_interior = numero_interior;
+	}
+	public String getColonia() {
+		return colonia;
+	}
+	public void setColonia(String colonia) {
+		this.colonia = colonia;
+	}
+	
 }
--- demo/src/test/java/com/example/demo/models/Person.java
+++ demo/src/test/java/com/example/demo/models/Person.java
@@ -1,5 +1,35 @@
 package com.example.demo.models;
 
 public class Person {
+	
+	public String primer_nombre;
+	public String segundo_nombre;
+	public String apellido_paterno;
+	public String apellido_materno;
+	
+	public String getPrimer_nombre() {
+		return primer_nombre;
+	}
+	public void setPrimer_nombre(String primer_nombre) {
+		this.primer_nombre = primer_nombre;
+	}
+	public String getSegundo_nombre() {
+		return segundo_nombre;
+	}
+	public void setSegundo_nombre(String segundo_nombre) {
+		this.segundo_nombre = segundo_nombre;
+	}
+	public String getApellido_paterno() {
+		return apellido_paterno;
+	}
+	public void setApellido_paterno(String apellido_paterno) {
+		this.apellido_paterno = apellido_paterno;
+	}
+	public String getApellido_materno() {
+		return apellido_materno;
+	}
+	public void setApellido_materno(String apellido_materno) {
+		this.apellido_materno = apellido_materno;
+	}
 
 }
